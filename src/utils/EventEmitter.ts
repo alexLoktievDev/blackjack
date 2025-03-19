b3e54fda7ca657da5d6a9e041f8a0ff3b7b9cd04
Alex <alexander.loktiev@techmagic.co>
2025-03-19 14:05:24 +0200

    Add EventEmitter.

---
+++ src/utils/EventEmitter.ts
@@ -0,0 +1,18 @@
+type Listener = (...args: any[]) => void;
+
+export class EventEmitter {
+  private events: { [key: string]: Listener[] } = {};
+
+  on(event: string, listener: Listener): void {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(listener);
+  }
+
+  emit(event: string, ...args: any[]): void {
+    if (this.events[event]) {
+      this.events[event].forEach((listener) => listener(...args));
+    }
+  }
+}
