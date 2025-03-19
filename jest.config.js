3504b40c6d95cbf128f51558783728bccd8084ab
Alex <alexander.loktiev@techmagic.co>
2025-03-19 15:09:40 +0200

    Fix GitHub Actions TypeScript setup.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom', // ✅ Use jsdom for browser-like environment
+  testEnvironment: 'jest-environment-jsdom',
   clearMocks: true,
   roots: ['<rootDir>/src'],
   moduleNameMapper: {
