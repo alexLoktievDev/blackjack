75856572297610a7b35e77a9899fd9e3b7fc4577
Alex <alexander.loktiev@techmagic.co>
2025-03-19 14:00:03 +0200

    Add BetManager.

---
+++ src/modules/bet-manager/BetManager.ts
@@ -0,0 +1,24 @@
+export class BetManager {
+  protected bet: number = 0;
+
+  placeBet(amount: number): boolean {
+    this.bet = amount;
+    return true;
+  }
+
+  winBet(): number {
+    return this.bet * 2;
+  }
+
+  loseBet(): void {
+    this.bet = 0;
+  }
+
+  getBet(): number {
+    return this.bet;
+  }
+
+  clear(): void {
+    this.bet = 0;
+  }
+}
